135ef44cdab8471f1a7fad729f7a7885ef84275d
Dries007 <admin@dries007.net>
2018-02-25 23:05:47 +0100

    attempt to fix #7

--- docker/getinfo.sh
+++ docker/getinfo.sh
@@ -13,28 +13,28 @@ FILE_PATH_INDEX="${PROJECT_PATH_1}/files/index.json"
 PROJECT_PATH_2="/www/${PROJECT_ID}"
 FILE_PATH_2="${PROJECT_PATH_2}/${FILE_ID}.json"
 
-echo "getinfo.sh request to $QUERY_STRING, parsed as $PROJECT_ID $FILE_ID" >> /var/log/cron.log
+echo "getinfo.sh request to $QUERY_STRING, parsed as $PROJECT_ID $FILE_ID" >>/var/log/getinfo.log 2>>/var/log/getinfo.error
 
 echo "Content-type: text/json"
 echo ""
 
-dotnet /alpacka-meta/out/alpacka-meta.dll get -o /data --filter None --project ${PROJECT_ID} 2>&1  >> /var/log/cron.log
-dotnet /alpacka-meta/out/alpacka-meta.dll get -o /data --filter None --file ${PROJECT_ID}-${FILE_ID} 2>&1  >> /var/log/cron.log
+dotnet /alpacka-meta/out/alpacka-meta.dll get -o /data --filter None --project ${PROJECT_ID} >>/var/log/getinfo.log 2>>/var/log/getinfo.error
+dotnet /alpacka-meta/out/alpacka-meta.dll get -o /data --filter None --file ${PROJECT_ID}-${FILE_ID} >>/var/log/getinfo.log 2>>/var/log/getinfo.error
 
 if [[ -f $FILE_PATH_INDEX ]]; then
-    python3 -m CurseMeta project $PROJECT_PATH_1 $PROJECT_PATH_2 2>&1 >> /var/log/cron.log
+    python3 -m CurseMeta project $PROJECT_PATH_1 $PROJECT_PATH_2 >>/var/log/getinfo.log 2>>/var/log/getinfo.error
 fi
 
 if [[ ! -f $FILE_PATH_2 ]]; then
-    python3 -m CurseMeta file $FILE_PATH_1 $FILE_PATH_2 2>&1 >> /var/log/cron.log
+    python3 -m CurseMeta file $FILE_PATH_1 $FILE_PATH_2 >>/var/log/getinfo.log 2>>/var/log/getinfo.error
 fi
 
 if [[ -f $FILE_PATH_2 ]]; then
     cat $FILE_PATH_2
-     echo "getinfo $QUERY_STRING OK" >> /var/log/cron.log
+     echo "getinfo $QUERY_STRING OK" >>/var/log/getinfo.log 2>>/var/log/getinfo.error
     exit 0
 else
-    echo "getinfo $QUERY_STRING ERROR" >> /var/log/cron.log
+    echo "getinfo $QUERY_STRING ERROR" >>/var/log/getinfo.log 2>>/var/log/getinfo.error
     echo "{ \"error\": true, \"code\": 404, \"message\": \"Not found on backend API\" }"
 fi
 
