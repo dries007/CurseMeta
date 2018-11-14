a6e44bb18a3c1dfd83349edecc391c0137aed44c
Dries007 <admin@dries007.net>
2018-11-14 22:38:24 +0100

    Now marking any 404 returning addon as deleted on 'all file' fetch to disable polling. If the addon is requested via other means it will get un-deleted.
    Signed-off-by: Dries007 <admin@dries007.net>

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,9 +1,12 @@
+import requests
+
 from CurseClient import MAX_ADDONS_PER_REQUEST
 
 from celery.utils.log import get_task_logger
 
 from .. import celery
-from ..models import AddonModel
+from .. import db
+from ..models import AddonModel, AddonStatusEnum
 from ..models import FileModel
 from ..helpers import get_curse_api
 from ..helpers import post_curse_api
@@ -20,6 +23,14 @@ def request_all_files(id_):
                 FileModel.update(id_, x)
             except:
                 logger.exception('All files request inner error on {}'.format(x))
+    except requests.HTTPError as e:
+        if e.response.status_code == 404:
+            logger.info('404 on addon {}. Setting status to deleted to disable further polling.'.format(id_))
+            x: AddonModel = AddonModel.query.get(id_)
+            x.status = AddonStatusEnum.Deleted
+            db.session.commit()
+        else:
+            logger.exception('All files request error on {}'.format(id_))
     except:
         logger.exception('All files request error on {}'.format(id_))
 
